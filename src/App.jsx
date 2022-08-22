03b0ff21047e297e307cf69e9774291fc543f2d1
Dmytro Barabash <dbarabashdev@gmail.com>
2022-08-22 23:16:43 +0300

    Added Prettier and Eslint

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,21 @@
-import { useState } from 'react'
+import {useState} from 'react'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
-    
+    <div>
       <h1>Vite + React</h1>
       <h3>by droptarget team</h3>
 
-      <div className="card">
+      <div>
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
-
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
+      </div>
     </div>
   )
 }
